9cb0abee71094a4cb5c91a028a8342fc83b15b8b
Bammez <Kingnuel4@gmail.com>
2024-05-03 14:45:29 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
     Sublist,
     Superlist,
     Unequal,
 }
-
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    if first_list == second_list {
+        Comparison::Equal
+    } else if first_list.is_empty() ||
+        second_list.windows(first_list.len()).any(|x| x == first_list) {
+        Comparison::Sublist
+    } else if second_list.is_empty() ||
+        first_list.windows(second_list.len()).any(|x| x == second_list) {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
+    }
 }
+
+
